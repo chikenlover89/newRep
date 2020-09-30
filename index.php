b296bb424f44f0342266e78d455c8b003a00c79c
Aivars <aivars.silis@gmail.com>
2020-09-30 11:51:12 +0300

    ar klasem

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ require_once 'NumberGenerator.php';
 require_once 'FileManager.php';
 
 $numberGenerator = new NumberGenerator();
+$num = $numberGenerator->getNumber(1,1000);
 $fileManager = new FileManager();
 
-$fileManager->addNumToFile($numberGenerator->getNumber(1,1000));
-echo $fileManager->getAverage()."\n";
+$fileManager->addNumToFile($num);
+echo "Number: $num\nAVG: ".$fileManager->getAverage()."\n";
